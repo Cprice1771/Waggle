2536ce3d479821ac608379d5b3aaae1d77639667
Hang Yu <yuangm@hotmail.com>
2014-03-18 15:22:01 -0400

    added a database file

---
+++ database/waggle_create_users.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE waggle;
+
+CREATE TABLE useres
+(
+FirstName varchar(45),
+LastName varchar(45),
+Email varchar(45),
+Password varchar(45),
+Verified tinyint(1),
+Banned tinyint(1),
+);
